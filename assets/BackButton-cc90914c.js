fc0e46edf70609190cdbdc8212c44348222ccd38
ImYersain <ersainaldabaev@gmail.com>
2023-10-30 20:36:03 +0100

    Updates

--- assets/BackButton-cc90914c.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as t,L as a}from"./index-c05b1200.js";const r=()=>{const{t:s}=n();return t.jsx(a,{to:"/",className:"button button--black",children:t.jsx("span",{children:s("buttons.back.label")})})};export{r as B};
